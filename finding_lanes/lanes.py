f6a94555d3e7ae27bc13431ed2baf9cda7549aab
Arun Sasi <aruntdpa@gmail.com>
2025-09-05 13:45:21 +0200

    applying Gaussian blur

--- finding_lanes/lanes.py
+++ finding_lanes/lanes.py
@@ -4,5 +4,6 @@ import numpy as np
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 grey = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('result', grey)
+blur = cv2.GaussianBlur(grey, (5,5), 0)
+cv2.imshow('result', blur)
 cv2.waitKey(0)
