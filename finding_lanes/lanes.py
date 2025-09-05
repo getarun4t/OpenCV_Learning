f19dcb6a2ed4c3e8cfcfe6fce4394e84b196fcb4
Arun Sasi <aruntdpa@gmail.com>
2025-09-05 15:05:31 +0200

    applying matplotlib

--- finding_lanes/lanes.py
+++ finding_lanes/lanes.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def canny(image):
-    grey = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+def get_canny(image):
+    grey = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(grey, (5,5), 0)
     return cv2.Canny(blur, 50, 150)
     
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cv2.imshow('result', canny)
-cv2.waitKey(0)
+canny = get_canny(lane_image)
+plt.imshow(canny)
+plt.show()
