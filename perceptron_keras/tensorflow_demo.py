6fffc6e73257f8e35e98a7289450740bb6f7e8a7
Arun Sasi <aruntdpa@gmail.com>
2025-09-12 11:51:35 +0200

    Train a model

--- perceptron_keras/tensorflow_demo.py
+++ perceptron_keras/tensorflow_demo.py
@@ -1,6 +1,10 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.models import Sequential
+#Importing instead of gradient algorithm since it is expensive
+from keras.optimizers import Adam  
 
 #%%
 n_pts = 500
@@ -11,8 +15,18 @@ Xb = np.array([np.random.normal(8, 2, n_pts),
                np.random.normal(6, 2, n_pts)]).T
  
 X = np.vstack((Xa, Xb))
-y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
+Y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
  
 plt.scatter(X[:n_pts,0], X[:n_pts,1])
 plt.scatter(X[n_pts:,0], X[n_pts:,1])
+
+# %%
+model = Sequential()
+model.add(Dense(units=1, input_shape=(2,), activation='sigmoid' ))
+#Adding optimizer
+adam = Adam(learning_rate = 0.1)
+#Set learning process
+model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
+#Train model to fit to our data
+model.fit(x=X, y=Y, verbose=1, batch_size=50, epochs=500, shuffle='true')
 # %%
