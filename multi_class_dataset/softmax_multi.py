065337aa5dfebca6232c5fcd71e0a81067241563
Arun Sasi <aruntdpa@gmail.com>
2025-09-18 17:17:16 +0200

    Plotting data

--- multi_class_dataset/softmax_multi.py
+++ multi_class_dataset/softmax_multi.py
@@ -11,3 +11,12 @@ n_pts = 500
 centers = [[-1, 1], [-1, -1], [1, -1]]
 # random_state allows to get same random data on rerun
 X, y = datasets.make_blobs(n_samples=n_pts, random_state=123, centers=centers, cluster_std= 0.4)
+print(X)
+print(y)
+# %%
+# plotting
+plt.scatter(X[y==0, 0], X[y==0, 1])
+plt.scatter(X[y==1, 0], X[y==1, 1])
+plt.scatter(X[y==2, 0], X[y==2, 1])
+# %%
+# Hot encoding, eliminates unnecessary relations b/w datasets
