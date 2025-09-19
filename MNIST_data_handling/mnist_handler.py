6849b8f92e39607793947448d2336abe172a6618
Arun Sasi <aruntdpa@gmail.com>
2025-09-19 13:04:17 +0200

    adding headers

---
+++ MNIST_data_handling/mnist_handler.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+# Importing mnist data set
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+# %%
