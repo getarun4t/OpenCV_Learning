fd64e464d33be12dea687284989ed7874506b666
Arun Sasi <aruntdpa@gmail.com>
2025-09-26 12:29:58 +0200

    Getting filename and histogram for steering angles

--- Behavioural Cloning/behavioural_cloning.py
+++ Behavioural Cloning/behavioural_cloning.py
@@ -21,4 +21,20 @@ data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names=columns)
 pd.set_option('display.max_colwidth', None)
 data.head()
 
+# Stripping file name from the path
+def path_leaf(path):
+    return os.path.basename(path)
+
+# Apply the function to all selected columns simultaneously
+for col in ['center', 'left', 'right']:
+    data[col] = data[col].apply(path_leaf)
+data.head()
+
+# %%
+# Plotting steering angle for visualization in histogram
+# For identifying the steering angle which is most common
+num_bins = 25
+# Getting histogram and bins
+hist, bins = np.histogram(data['steering'], num_bins)
+print(bins)
 # %%
