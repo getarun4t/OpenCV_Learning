c3ffe7ae652c6a1482e0bf5dd05adbdc2f92b32f
Arun Sasi <aruntdpa@gmail.com>
2025-09-29 10:44:20 +0200

    Importing header and setting basics

--- Behavioural Cloning/drive.py
+++ Behavioural Cloning/drive.py
@@ -1,15 +1,26 @@
 # Creating a connection between model and simulator
+
+# for webserver
+# For real time connection between client and server
+import socketio
+# For event listening socket
+import eventlet
 # Flask - Python micro framework for initilizing webapp
 from flask import Flask
+
+# Creating a socketio web server
+# Requires a middleware for trafficing data to it
+# Flask used for it
+sio = socketio.Server()
 
 # replaces main
 app = Flask(__name__)  
 
-# Router decorator
-# If user routes to this location in browser, it will displays welcome
-@app.route('/home')
-def greeting():
-    return 'Welcome !'
+@sio.on('connect') #message, disconnect
+def connect(sid, environ):
+    print('Connected')
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    # Setting flask app as middleware
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
