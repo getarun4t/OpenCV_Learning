fa52a53d3916d33cd531b46978eea4e1d4f8a42c
Arun Sasi <aruntdpa@gmail.com>
2025-10-06 14:41:46 +0200

    Creating data sets

--- PyTorch-Linear Regression/custom_modules.py
+++ PyTorch-Linear Regression/custom_modules.py
@@ -2,6 +2,20 @@
 # Headers 
 import torch
 import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+# Creating a dataset
+# randn returns a tensor filled with random numbers distributed
+# 100 points with normal distriubution of 1
+X = torch.randn(100, 1)
+# Adding noise by distributing the Y coordinates
+y = X + 3 * torch.randn(100, 1)
+plt.plot(X.numpy(), y.numpy(), 'o')
+plt.xlabel("x")
+plt.ylabel("y")
+
 
 #%%
 # A linear model template
@@ -21,14 +35,27 @@ class LR(nn.Module):
 # Creating a new linear model
 torch.manual_seed(1)
 model = LR(1,1)
+print(model)
 
 #%%
-# Using forward class
-x = torch.tensor([1.0])
-print(model.forward(x))
+# Getting model parameters
+[w, b] = model.parameters()
+# Fetching model parameters
+# Using a func
+def get_params():
+    return (w[0][0].item(), b[0].item())
 
-# Checking with multiple inputs
-x = torch.tensor([[1.0], [2.0]])
-print(model.forward(x))
+#%%
+# Plotting the params
+def plt_fit(title):
+    plt.title = title
+    w1, b1 = get_params()
+    x1 = np.array([-30, 30])
+    y1 = w1*x1 + b1
+    plt.plot(x1, y1, 'r')
+    plt.scatter(X, y)
+    plt.show()
+
+plt_fit('Initial Model')
 
 #%%
