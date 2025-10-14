7c70510929eb9be8796f0960b9da9590ef44da5a
Arun Sasi <aruntdpa@gmail.com>
2025-10-14 16:36:29 +0200

    Getting the MNIST data

--- PyTorch-MNIST/pytorch_mnist_data_handling.py
+++ PyTorch-MNIST/pytorch_mnist_data_handling.py
@@ -7,3 +7,10 @@ from torchvision import datasets, transforms
 
 #%%
 # Getting MNIST data set
+# Converting to tensor using transform
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 1), (0.5, 0.5, 0.5))
+                                ])
+training_dataset = datasets.MNIST(root = './data', train = True, download= True, transform=transform)
+print(training_dataset)
+
+#%%
