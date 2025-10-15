bcd01d5588dfd9211284f7471ce7f385ff7366cd
Arun Sasi <aruntdpa@gmail.com>
2025-10-15 15:13:01 +0200

    Fixing the issues

--- PyTorch-MNIST/pytorch_mnist_data_handling.py
+++ PyTorch-MNIST/pytorch_mnist_data_handling.py
@@ -3,13 +3,16 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+
 from torchvision import datasets, transforms
 
 #%%
 # Getting MNIST data set
 # Converting to tensor using transform
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 1), (0.5, 0.5, 0.5))
-                                ])
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
 training_dataset = datasets.MNIST(root = './data', train = True, download= True, transform=transform)
 # Cutting to smaller chunks
 training_loader = torch.utils.data.DataLoader(dataset=training_dataset, batch_size=100, shuffle=True)
@@ -29,7 +32,7 @@ def im_convert(tensor):
 #%%
 # Creating an iterable
 data_iter = iter(training_loader)
-images, labels = data_iter.next()
+images, labels = next(data_iter)
 fig = plt.figure(figsize=(25, 4))
 
 for idx in np.arange(20):
