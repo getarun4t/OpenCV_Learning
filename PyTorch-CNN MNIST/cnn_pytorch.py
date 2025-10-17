128f9ef6b1679f736c394cc858f727c79c536dfd
Arun Sasi <aruntdpa@gmail.com>
2025-10-17 16:17:26 +0200

    Copying base code

---
+++ PyTorch-CNN MNIST/cnn_pytorch.py
@@ -0,0 +1,191 @@
+#%%
+# Headers
+import torch
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn.functional as F
+import PIL
+from torch import nn
+from torchvision import datasets, transforms
+from PIL import Image
+
+#%%
+# Getting MNIST data set
+# Converting to tensor using transform
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+# Creating training and validation dataset
+training_dataset = datasets.MNIST(root = './data', train = True, download= True, transform=transform)
+validation_dataset = datasets.MNIST(root = './data', train = False, download= True, transform=transform)
+# Cutting to smaller chunks
+training_loader = torch.utils.data.DataLoader(dataset=training_dataset, batch_size=100, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=100, shuffle=False)
+
+#%%
+# Changing to numpy array
+def im_convert(tensor):
+    image = tensor.clone().detach().numpy()
+    # Getting 28*28*1 shape
+    image = image.transpose (1, 2, 0)
+    # Normalizing the image
+    image = image* np.array((0.5, 0.5, 0.5))+ np.array((0.5, 0.5, 0.5))
+    # Clipping the image
+    image = image.clip(0,1)
+    return image
+
+#%%
+# Creating an iterable
+data_iter = iter(training_loader)
+images, labels = next(data_iter)
+fig = plt.figure(figsize=(25, 4))
+
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 10, idx+1)
+    plt.imshow(im_convert(images[idx]))
+    ax.set_title([labels[idx].item()])
+
+#%%
+# Creating a neural network class
+class Classifier(nn.Module):
+    def __init__(self, D_in, H1, H2, D_out):
+        super().__init__()
+        # Input layer
+        self.linear1 = nn.Linear(D_in, H1)
+        # First hidden layer
+        self.linear2 = nn.Linear(H1, H2)
+        # Second hidden layer
+        self.linear3 = nn.Linear(H2, D_out)
+    
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        # No activation required in last layer
+        # Output returned is raw output
+        x = self.linear3(x)
+        return x
+
+# Setting hidden layer dimensions during init    
+model = Classifier(784,125, 65, 10)
+print(model)
+
+#%%
+# Getting the loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+#%%
+# Setting the number of epochs
+epochs = 12
+running_loss_history = []
+running_correct_history = []
+validation_loss_history = []
+validation_correct_history = []
+
+for e in range(epochs):
+    running_loss = 0.0
+    running_correct = 0.0
+
+    validation_running_loss = 0.0
+    validation_running_correct = 0.0
+
+    for images, labels in training_loader:
+        # Reshaping the training 
+        inputs = images.view(images.shape[0], -1)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        _, preds = torch.max(outputs, 1)
+
+        running_loss+=loss.item()
+        running_correct+=torch.sum(preds == labels.data)
+    else:
+        with torch.no_grad():
+            for val_inputs, val_labels in validation_loader:
+                val_inputs = val_inputs.view(val_inputs.shape[0], -1)
+                val_outputs = model(val_inputs)
+                val_loss = criterion(val_outputs, val_labels)
+
+                _, val_preds = torch.max(val_outputs, 1)
+
+                validation_running_loss+=val_loss.item()
+                validation_running_correct+=torch.sum(val_preds == val_labels.data)
+        epoch_loss = running_loss/len(training_loader)
+        epoch_acc = running_correct.float()/len(training_loader)
+        running_loss_history.append(epoch_loss)
+        running_correct_history.append(epoch_acc)
+        
+        val_epoch_loss = validation_running_loss/len(validation_loader)
+        val_epoch_acc = validation_running_correct.float()/len(validation_loader)
+        validation_loss_history.append(val_epoch_loss)
+        validation_correct_history.append(val_epoch_acc)
+
+        print(f'epoch: {e+1}')
+        print(f'Training loss : {epoch_loss}, Training Accuracy: {epoch_acc.item()}')
+        print (f'Validation loss: {val_epoch_loss}, Validation Accuracy: {val_epoch_acc.item()}')
+
+#%%
+# Plotting loss
+plt.plot(running_loss_history, label='training loss')
+plt.plot(validation_loss_history, label='Validation loss')
+
+#%%
+# Plotting accuracy
+plt.plot(running_correct_history, label='Accuracy')
+plt.plot(validation_correct_history, label='Validation Accuracy')
+
+# %%
+# Getting test image from web
+url = 'https://images.homedepot-static.com/productImages/007164ea-d47e-4f66-8d8c-fd9f621984a2/svn/architectural-mailboxes-house-letters-numbers-3585b-5-64_1000.jpg'
+response = requests.get(url, stream=True)
+img = Image.open(response.raw)
+plt.imshow(img)
+
+#%%
+# Preprocessing the image to model input format 
+# Change image to black background and white digit
+img = PIL.ImageOps.invert(img)
+# Converting the image to single channel
+img = img.convert('L')
+# Transforming to 28*28
+img = img.resize((28, 28)) 
+
+# Transform through same MNIST preprocessing 
+img = transform(img)
+plt.imshow(im_convert(img))
+
+#%%
+# Adding to the model, flattening and predicting
+img = img.view(img.shape[0], -1)
+output = model(img)
+_, pred = torch.max(output, 1)
+print(pred.item())
+
+# %%
+# Validation iter
+# Creating an iterable
+data_iter = iter(validation_loader)
+images, labels = next(data_iter)
+
+# ✅ Keep original for display
+images_display = images.clone()
+
+# ✅ Flatten ONLY for prediction
+inputs = images.view(images.shape[0], -1)
+output = model(inputs)
+_, preds = torch.max(output, 1)
+
+fig = plt.figure(figsize=(25, 4))
+
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 10, idx+1)
+    plt.imshow(im_convert(images_display[idx]))
+    correct = preds[idx] == labels[idx]
+    ax.set_title(f"{preds[idx].item()} ({labels[idx].item()})", color="green" if correct else "red")
+    ax.axis("off")
+# %%
